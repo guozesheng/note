b597c4a62808f78fb254e2ee2fbf4c44f02cba9b
Guo Zesheng <guozesheng@gmail.com>
2014-12-11 00:09:37 +0800

    add something for install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install vim
-sudo apt-get install git
-sudo apt-get install tig
+sudo apt-get -y upgrade
+sudo apt-get -y install vim
+sudo apt-get -y install git
+sudo apt-get -y install tig
+sudo apt-get -y install fcitx
+sudo apt-get -y install fcitx-table-wubi
+sudo apt-get -y install smplayer
+sudo apt-get -y install qmmp
+sudo apt-get -y install ttf-wqy-zenhei
+sudo apt-get -y install exuberant-ctags ack-grep
+sudo apt-get -y install unrar
+sudo apt-get -y install p7zip
 
 git clone git://github.com/happypeter/peter-vim.git
 mv peter-vim ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
-sudo apt-get install exuberant-ctags ack-grep
 echo "" >> ~/.vimrc
 echo "  set nu" >> ~/.vimrc
 
@@ -33,3 +40,5 @@ echo "  template = ./.commit-template" >> $GITCONF
 echo "[push]" >> $GITCONF
 echo "  default = current" >> $GITCONF
 
+sudo apt-get -y autoremove
+sudo apt-get -y clean
